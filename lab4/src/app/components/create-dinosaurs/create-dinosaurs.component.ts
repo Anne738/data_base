133da50ff840a54e0f42e87b8bdc399c810cc918
Anne738 <94481296+Anne738@users.noreply.github.com>
2024-05-13 00:51:13 +0300

    add fix in lab4

---
+++ lab4/src/app/components/create-dinosaurs/create-dinosaurs.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Dinosaur} from "../../model/model";
+import {DinoService} from "../../services/dino.service";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-dinosaurs',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './create-dinosaurs.component.html',
+  styleUrl: './create-dinosaurs.component.css'
+})
+export class CreateDinosaursComponent {
+  @Output() onSubmit: EventEmitter<void> = new EventEmitter();
+
+  newDino: Dinosaur = {
+    name: '',
+    type: '',
+    age: 0
+  };
+
+  constructor(private dinoService: DinoService) { }
+
+
+  createDino(): void {
+    this.dinoService.createDino(this.newDino)
+      .subscribe(dino => {
+        console.log('Dinosaur created:', dino);
+        this.onSubmit.emit()
+      });
+  }
+}
